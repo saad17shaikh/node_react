2260ed805c6de20eab560c3ff5c028ba597e6c38
Saad Shaikh <saad17shaikh@gmail.com>
2024-01-17 21:57:25 +0530

    basic node and react setup with vite and tailwind

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import "dotenv/config";
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello App is running on port 5000");
+});
+
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
+});
+console.log("hello");
---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className='h-screen bg-slate-800'>
+        <p  className='text-xl text-gray-200 text-center '>Hello I am Saad Shaikh</p>
+    </div>
+  )
+}
+
+export default App
